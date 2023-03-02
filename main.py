877c9173ac417deb6b906d5ee8af01a1995558ec
Hariri <seyarserathariri@gmail.com>
2023-03-02 17:41:03 -0500

    Capturing the object and send through the email.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
 
 
@@ -8,6 +10,14 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(images)
+
 
 while True:
     status = 0
@@ -34,12 +44,19 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"q/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
 
-    if status_list[0] == 1 and status_list[1] ==0:
-        send_email()
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email(image_with_object)
+        clean_folder()
 
     cv2.imshow("video", frame)
 
